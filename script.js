d7bbcd1cefd00524a7dfc8bc83630e738673b820
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-07-26 22:13:48 +0600

    jquery plugin and javascript added and this is the final commmit of this project

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-/* 
-To open and close , we can use click function in btn
-and slideToggle() function to the desired element
-*/
-
 $(function() {
     $(".btn").click(function() {
         $(".submenu").slideToggle(500);
     });
-});
-
+}); 
